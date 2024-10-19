982dc2c05896a8ccacc6519aa774db6b68126d48
Natalia Repkina <pn.88@mail.ru>
2024-10-19 18:59:12 +0300

    feat: start gallery animation

---
+++ js/carousel.js
@@ -0,0 +1,55 @@
+let position = 0;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselButtons = document.querySelectorAll(
+    ".gallery-carousel__button"
+  );
+  carouselButtons.forEach((button) => {
+    button.addEventListener("click", (event) =>
+      handlerClickOnCarouselButtons(event)
+    );
+  });
+});
+
+function handlerClickOnCarouselButtons(event) {
+  const button = event.target;
+  const carousel = button.closest(".gallery-carousel");
+  const carouselItemsList = carousel.querySelector(
+    ".gallery-carousel__items-list"
+  );
+  const direction = button.classList.contains("next-button") ? "left" : "right";
+
+  const wrapperWith = carousel.querySelector(
+    ".gallery-carousel__wrapper"
+  ).offsetWidth;
+  if (direction == "left") {
+    position -= wrapperWith;
+  } else {
+    position += wrapperWith;
+  }
+
+  rotateCarousel(carouselItemsList, position);
+}
+
+function rotateCarousel(carousel, position) {
+  carousel.style.transform = `translateX(${position}px)`;
+
+  /* const wrapper = carousel.querySelector(".gallery-carousel__wrapper");
+  const itemWith = carousel.querySelector(
+    ".gallery-carousel__item"
+  ).offsetWidth;
+  const wrapperWith = wrapper.offsetWidth;
+
+  console.log(wrapperWith);
+
+  const carouselItemsList = carousel.querySelector(
+    ".gallery-carousel__items-list"
+  );
+  const position =
+    parseInt(getComputedStyle(carouselItemsList).transform) != NaN
+      ? parseInt(getComputedStyle(carouselItemsList).transform)
+      : 0;
+
+  console.log(getComputedStyle(carouselItemsList).transform);
+  */
+}
