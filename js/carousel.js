55ccb84b938b75ce83d9d63cf443a2c9fe93f453
Natalia Repkina <pn.88@mail.ru>
2024-10-21 13:14:27 +0300

    fix: fix logic bugs

--- js/carousel.js
+++ js/carousel.js
@@ -1,79 +1,86 @@
-let position = 0;
-
 document.addEventListener("DOMContentLoaded", function () {
-  const carouselButtons = document.querySelectorAll(
-    ".gallery-carousel__button"
-  );
-  carouselButtons.forEach((button) => {
-    button.addEventListener("click", (event) =>
-      handlerClickOnCarouselButtons(event)
+  // Все карусели на странице
+  const carousels = document.querySelectorAll(".gallery-carousel");
+
+  // Установка анимации для каждой карусели
+  carousels.forEach((carousel) => {
+    let position = 0;
+
+    // Тип карусели
+    const isAutoplay = carousel.dataset.autoplay === "true";
+    const isLoop = carousel.dataset.loop === "true";
+
+    // Основные DOM елементы карусели
+    const buttonNext = carousel.querySelector(".next-button");
+    const buttonPrev = carousel.querySelector(".prev-button");
+    const carouselItemsList = carousel.querySelector(
+      ".gallery-carousel__items-list"
     );
+
+    // Размеры элементов
+    const wrapperWidth = carousel.querySelector(
+      ".gallery-carousel__wrapper"
+    ).offsetWidth;
+    const itemsListWidth = carouselItemsList.offsetWidth;
+    const gap = parseInt(getComputedStyle(carouselItemsList).gap) || 0;
+
+    // Функция для обработки кликов по кнопкам
+    function handleClick(direction) {
+      // Изменение позиции
+      if (direction === "next") {
+        position -= wrapperWidth + gap;
+      } else if (direction === "prev") {
+        position += wrapperWidth + gap;
+      }
+      position = Math.max(
+        Math.min(position, 0),
+        -(itemsListWidth - wrapperWidth)
+      );
+
+      // Прокрутка карусели
+      rotateCarousel(carouselItemsList, position);
+
+      // Дисейбл кнопок в случае крайних положений
+      setNavigationButtons(
+        position,
+        itemsListWidth,
+        wrapperWidth,
+        buttonNext,
+        buttonPrev
+      );
+    }
+
+    // Обработчики событий по кликам на кнопки
+    buttonNext.addEventListener("click", () => handleClick("next"));
+    buttonPrev.addEventListener("click", () => handleClick("prev"));
+
+    // Начальные состояния кнопок
+    buttonPrev.disabled = true;
   });
 });
-
-function handlerClickOnCarouselButtons(event) {
-  // DOM Elements
-  const button = event.target;
-  const carousel = button.closest(".gallery-carousel");
-  const buttonNext = carousel.querySelector(".next-button");
-  const buttonPrev = carousel.querySelector(".prev-button");
-  const carouselItemsList = carousel.querySelector(
-    ".gallery-carousel__items-list"
-  );
-
-  const wrapperWidth = carousel.querySelector(
-    ".gallery-carousel__wrapper"
-  ).offsetWidth;
-  const itemsListWidth = carouselItemsList.offsetWidth;
-  const gap = parseInt(getComputedStyle(carouselItemsList).gap);
-
-  const direction = button.classList.contains("next-button") ? "left" : "right";
-
-  if (direction == "left") {
-    position -= wrapperWidth + gap;
-  } else {
-    position += wrapperWidth + gap;
-  }
-
-  toggleNavigationButtons(
-    position,
-    itemsListWidth,
-    wrapperWidth,
-    buttonNext,
-    buttonPrev
-  );
-
-  rotateCarousel(carouselItemsList, position);
-
-  toggleNavigationButtons(
-    position,
-    itemsListWidth,
-    wrapperWidth,
-    buttonNext,
-    buttonPrev
-  );
-}
 
 function rotateCarousel(carousel, position) {
   carousel.style.transform = `translateX(${position}px)`;
 }
 
-function toggleNavigationButtons(
+function setNavigationButtons(
   position,
   itemsListWidth,
   wrapperWidth,
   buttonNext,
   buttonPrev
 ) {
-  if (itemsListWidth + (position - wrapperWidth) <= 0) {
-    buttonNext.disabled = true;
-  } else {
-    buttonNext.disabled = false;
-  }
-
+  // Начало карусели
   if (position === 0) {
     buttonPrev.disabled = true;
   } else {
     buttonPrev.disabled = false;
+  }
+
+  // Конец карусели
+  if (position <= -(itemsListWidth - wrapperWidth)) {
+    buttonNext.disabled = true;
+  } else {
+    buttonNext.disabled = false;
   }
 }
